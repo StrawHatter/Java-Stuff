4c961004734c8e33b79b05f86c3339e54b90a793
Paul Dreczkowski <StrawHatter@users.noreply.github.com>
2015-11-30 16:47:03 +0000

    Update BiteXOR.java

--- BiteXOR.java
+++ BiteXOR.java
@@ -2,14 +2,14 @@ public class ByteXOR {
 
     public static void main(String args[]) {
 	
-	    //Part 2 
-		byte a = (byte) 200;
-		byte k = (byte) 40;
+	//Part 2 
+	byte a = (byte) 200;
+	byte k = (byte) 40;
 		
-		byte c = (byte)(a ^ k);
-		byte d = (byte)(c ^ k);
+	byte c = (byte)(a ^ k);
+	byte d = (byte)(c ^ k);
 		
-		System.out.printf("dec a: %d %n", a);
+	System.out.printf("dec a: %d %n", a);
         System.out.printf("dec k: %d %n", k);
         System.out.printf("dec c: %d %n", c);
         System.out.printf("dec d: %d %n", d);
@@ -19,12 +19,12 @@ public class ByteXOR {
         System.out.printf("hex c: %H %n", c & 0XFF);
         System.out.printf("hex d: %H %n", d & 0XFF);
 		
-		System.out.printf("binary a:%s %n", Integer.toBinaryString(a & 0xFF));
+	System.out.printf("binary a:%s %n", Integer.toBinaryString(a & 0xFF));
         System.out.printf("binary k:%s %n", Integer.toBinaryString(k & 0xFF));
         System.out.printf("binary c:%s %n", Integer.toBinaryString(c & 0xFF));
         System.out.printf("binary d:%s %n", Integer.toBinaryString(d & 0xFF));
 		
-		// A and D both output the same regardless of it being a bin, dec, or hex
+	// A and D both output the same regardless of it being a bin, dec, or hex
 		
 	}
 }
